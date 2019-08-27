3f1fbec454708f7b2a67583e692aca669f1b9fd2
kralinc <legolink25@gmail.com>
2019-08-27 13:51:50 -0500

    dev

--- index.php
+++ index.php
@@ -20,23 +20,49 @@
       <div class="logo">
         <img src="img/compile-logo.png" id="compile-logo-center">
       </div>
-      <p class="h1">The ICC Information Technology Club</p>
+      <p class="h1">The ICC Information Technology RSO</p>
     </div>
 
-    <div class="container mono" id="column-right">
-      <p class="h1 text-center">Projects</p>
-      <p class="h3 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas egestas accumsan purus, quis porta purus commodo et. Integer aliquam tempus sem euismod placerat. Nulla a facilisis tellus. Integer commodo interdum posuere. Quisque lacinia magna odio, eu pellentesque lacus semper et.</p>
-    </div>
+   <div class="container-fluid" id="main-content">
 
-    <!--The plan is to have a 2 column layout of information on our front page, which will collapse into a single column when the screen width is small enough.-->
-    <div class="container mono content-main" id="about">
-      <p class="h1 text-center">About</p>
-      <p class="h3 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas egestas accumsan purus, quis porta purus commodo et. Integer aliquam tempus sem euismod placerat. Nulla a facilisis tellus. Integer commodo interdum posuere. Quisque lacinia magna odio, eu pellentesque lacus semper et. Praesent sit amet massa elit. Pellentesque sem risus, facilisis et scelerisque vel, posuere sit amet magna. Vestibulum eget ante massa. Quisque ullamcorper, leo vel pharetra dictum, mauris justo blandit risus, id viverra ipsum tellus quis orci. Nullam lobortis, sem non vestibulum malesuada, eros diam commodo mauris, vel faucibus eros nunc eu dui. Nulla lobortis eu felis vel dignissim. Morbi mattis justo volutpat nunc rhoncus euismod.</p>
-    </div>
+	    <div class="row">
 
-    <div class="container mono content-main" id="contact">
-      <p class="h1 text-center">Contact</p>
-      <p class="h3 text-center">Nullam consequat nibh vitae leo semper, ut sodales lacus condimentum. Curabitur et nibh nec nibh varius suscipit et eu ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris consectetur sapien quis pretium semper. Praesent varius felis a metus pretium, aliquet tempus nulla aliquet. Vestibulum vel dictum tellus, quis ultricies purus. Nam vitae dictum tortor, eget semper ipsum.</p>
+		    <div class="col-lg-8">
+			    <div class="container-fluid mono" id="about">
+			      <p class="h1">About</p>
+			      <p class="h2">Meetings: Wednesdays, 3pm room TBD</p>
+			      <p class="h3">The Computer Information League at ICC is dedicated to connecting Information Technology majors and those with interest through team projects and events. We want to create an environment where everyone can develop, and create projects from which everyone can learn. This website is still under construction, and contributions are welcome! A link to our github page is located in the website's footer.</p>
+			    </div>
+	     </div>
+
+	      <div class="col-lg-4">
+			    <div class="container-fluid mono">
+			      <p class="h1">Projects</p>
+			      <p class="h3">Coming Soon! Join our discord server or attend meetings for updates.</p>
+			    </div>
+	      </div>
+
+	    </div>
+
+
+
+	    <div class="row">
+	      <div class="col-lg-6">
+			    <div class="container-fluid mono" id="contact">
+			      <p class="h1">Leadership</p>
+			      <p class="h3">Advisor: Dr. Rafeeq Al-Hashemi</p>
+			      <p class="h3">President: <a href="mailto:la654@lab.icc.edu">Liam Andrade</a></p>
+			      <p class="h3">Vice President: Eli Mathis</p>
+			      <p class="h3">Secretary: Kaitelin Reys</p>
+			      <p class="h3">Treasurer: Tim Lance</p>
+			    </div>
+	      </div>
+
+        <!--<div class="col-lg-6">
+					
+        </div>-->
+	    </div>
+
     </div>
 
  <?php include("includes/footer.html");?>
